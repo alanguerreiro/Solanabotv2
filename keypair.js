1a480e5c502447216d07c731da436fd2c6694dcc
alanguerreiro <alan.guerreiroii@gmail.com>
2025-06-16 23:01:40 -0400

    Update keypair.js

--- keypair.js
+++ keypair.js
@@ -1,22 +1,20 @@
- keypair.js
-
 const fs = require('fs');
 const bs58 = require('bs58');
 const { Keypair } = require('@solana/web3.js');
 
-// 🔐 Sua chave privada em Base58:
+// 🔐 Sua chave privada em Base58 (NÃO é a seed, é a private key):
 const PRIVATE_KEY_BASE58 = '51dyWaLNZYpTuV4dwznxvPcyuMCMujfu7uUXfvgxodRY5UE8poGEyUPfPJW5aS9SgqZdeKLBQnXwFxounYRCQCXh';
 
 try {
-  // Decode da chave
+  // Decodifica a chave
   const secretKey = bs58.decode(PRIVATE_KEY_BASE58);
 
-  // Cria o keypair com a chave decodificada
+  // Cria o keypair
   const keypair = Keypair.fromSecretKey(secretKey);
 
-  // Salva no formato correto em keypair.json
+  // Salva em formato .json
   fs.writeFileSync('keypair.json', JSON.stringify(Array.from(keypair.secretKey)));
-  console.log('✅ Arquivo keypair.json gerado com sucesso!');
+  console.log('✅ keypair.json gerado com sucesso!');
 } catch (err) {
   console.error('❌ Erro ao gerar o keypair:', err.message);
 }
